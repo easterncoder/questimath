b67200e1a45f4a8db8136c3d25387df835837461
Mike Lopez <e@mikelopez.com>
2026-05-23 00:33:56 +0800

    chore: release 0.2.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'questimath-0.2.5';
+const CACHE_VERSION = 'questimath-0.2.6';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const DEPENDENCY_CACHE = `${CACHE_VERSION}-dependencies`;
